bd61a3bd38aebbe13c603fa4a198d64336b46ff2
Nikita <nikitbar@yandex.ru>
2021-10-21 21:34:41 +0300

    First commit

---
+++ app/src/main/java/com/nbarabanov/loftmoney/AddItemActivity.java
@@ -0,0 +1,14 @@
+package com.nbarabanov.loftmoney;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item_activity);
+    }
+}
